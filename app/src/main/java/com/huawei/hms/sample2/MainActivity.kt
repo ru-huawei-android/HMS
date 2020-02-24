ea25b13c2ca57bafc40f364b85b79af9ec71c4be
c7j <jigrakov@bakapp.ru>
2020-02-24 17:49:31 +0300

    analytics & crash sample

---
+++ app/src/main/java/com/huawei/hms/sample2/MainActivity.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package com.huawei.hms.sample2
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+//::created by c7j at 18.02.2020 13:07
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var hiAnalyticsWrapper: HiAnalyticsWrapper
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        hiAnalyticsWrapper = HiAnalyticsWrapper(applicationContext)
+        with(hiAnalyticsWrapper) {
+            setUpUserId()
+            reportScreen(this@MainActivity, javaClass.simpleName)
+            reportEventNoBundle()
+            reportEventStringBundle()
+            reportEventDeviceInfoBundle()
+        }.also { log("sending events") }
+
+        btnForceCrash.setOnClickListener { throw RuntimeException() }
+    }
+}
+
+
+fun log(message: Any?) = Log.e("#TEST", "$message")
