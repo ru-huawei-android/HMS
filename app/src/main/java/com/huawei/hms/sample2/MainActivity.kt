63edd7aac5f677c63d33db51fe89b436b7b62573
c7j <jigrakov@bakapp.ru>
2020-04-28 23:24:37 +0300

    update ui

--- app/src/main/java/com/huawei/hms/sample2/MainActivity.kt
+++ app/src/main/java/com/huawei/hms/sample2/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         hiAnalyticsWrapper = HiAnalyticsWrapper(applicationContext)
         with(hiAnalyticsWrapper) {
             setUpUserId()
